f222386d08134bc437ba4e1dec616d4b2b12bc3d
inparia <inparia@live.ca>
2020-12-17 02:50:23 -0500

    Added comments for information

--- GAME2014-F2020-FinalTest/Assets/_Scripts/FloatingEffect.cs
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/FloatingEffect.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 public class FloatingEffect : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    // Saves the original position.
     float originalY;
 
+    //Sets its floating strength. It can also be set in editor.
     public float floatStrength = 0.3f;
     void Start()
     {
+        //Sets the original position to current position.
         this.originalY = this.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Once it float up, then it float down.
         transform.position = new Vector3(transform.position.x,
             originalY + ((float)Math.Sin(Time.time) * floatStrength),
             transform.position.z);
--- GAME2014-F2020-FinalTest/Assets/_Scripts/ShrinkingPlatform.cs
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/ShrinkingPlatform.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class ShrinkingPlatform : MonoBehaviour
 {
-
+    //Checkers for if platform is going to shrink or not.
     public bool willShrink;
 
     
@@ -19,17 +19,19 @@ public class ShrinkingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // && gameObject.GetComponentInParent<MovingPlatformController>().isActive
+        // checks if player is on the platform, and it is set to shrink.
         if (willShrink && gameObject.GetComponentInParent<MovingPlatformController>().isActive)
         {
             if (gameObject.transform.localScale.x >= 0.0f)
             {
+                //If it's scale is greater than 0, decrement its scale.
                 gameObject.transform.localScale -= new Vector3(0.1f * Time.deltaTime, 0.1f * Time.deltaTime, 0);
             }
 
         }
         else
         {
+            //If it's scale is less than 0, increment its scale.
             if (gameObject.transform.localScale.x <= 1.0f)
             {
                 gameObject.transform.localScale += new Vector3(0.1f * Time.deltaTime, 0.1f * Time.deltaTime, 0);
