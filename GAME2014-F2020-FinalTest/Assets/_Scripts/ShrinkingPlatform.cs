dbc40ea9ebd83e981c0f4721e57aaba14de13176
inparia <inparia@live.ca>
2020-12-17 15:06:13 -0500

    Added sound when the platform shrink and expand

--- GAME2014-F2020-FinalTest/Assets/_Scripts/ShrinkingPlatform.cs
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/ShrinkingPlatform.cs
@@ -9,11 +9,14 @@ public class ShrinkingPlatform : MonoBehaviour
     //Checkers for if platform is going to shrink or not.
     public bool willShrink;
 
-    
+    public AudioSource[] sounds;
 
+    private bool isShrinking, isExpanding;
+    
     // Start is called before the first frame update
     void Start()
     {
+        isShrinking = false;
     }
 
     // Update is called once per frame
@@ -22,15 +25,24 @@ public class ShrinkingPlatform : MonoBehaviour
         // checks if player is on the platform, and it is set to shrink.
         if (willShrink && gameObject.GetComponentInParent<MovingPlatformController>().isActive)
         {
+            if(!isShrinking)
+            {
+                sounds[0].Play();
+                isShrinking = true;
+            }
             if (gameObject.transform.localScale.x >= 0.0f)
             {
                 //If it's scale is greater than 0, decrement its scale.
                 gameObject.transform.localScale -= new Vector3(0.1f * Time.deltaTime, 0.1f * Time.deltaTime, 0);
             }
-
         }
         else
         {
+            if (isShrinking)
+            {
+                sounds[1].Play();
+                isShrinking = false;
+            }
             //If it's scale is less than 0, increment its scale.
             if (gameObject.transform.localScale.x <= 1.0f)
             {
